3d52362cfe6066e4733d88e45300a9d1c8d70d0e
Adeel Zahid <m.adeel2144@gmail.com>
2020-07-29 02:48:58 +0500

    data transfer to Question Card.

--- src/API.ts
+++ src/API.ts
@@ -1,5 +1,16 @@
+import {shuffleArray} from './utils';
 
-export {};
+
+export type Question = {
+    category: string,
+    correct_answer: string,
+    difficulty: string,
+    incorrect_answers: string[],
+    question: string,
+    type: string
+}
+
+export type QuestionState = Question & {answers: string[]};
 
 export enum Difficulty {
     EASYY = 'easy',
@@ -14,5 +25,11 @@ export const FetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     console.log(difficulty, amount)
     const endPoint = `https://opentdb.com/api.php?amount=${amount}&category=22&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endPoint)).json();
-    console.log(data);
+    
+    return data.results.map((question: Question) => ({
+                ...question,
+                answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+            }
+        )
+    )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import React, {useState} from 'react';
 import {FetchQuizQuestions} from './API';
 import {Difficulty} from './API';
-// import QuestinCard from './components/QuestionCard';
+import QuestinCard from './components/QuestionCard';
+//types
+import {QuestionState} from './API';
+
+
+type AnswerObject = {
+  question:string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 
 const TOTAL_QUESTION = 10;
 
 function App() {
-  // const [loading, setLoading] = useState(false);
-  // const [questions, setQuestions] = useState([]);
-  // const [number, setNumber] = useState(0);
-  // const [userAnswer, setUserAnswer] = useState([]);
-  // const [score, setScore] = useState(0);
-  // const [gameOver, setGameOver] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+   const [gameOver, setGameOver] = useState(true);
 
-  console.log(FetchQuizQuestions(TOTAL_QUESTION, Difficulty.EASYY))
+  console.log(questions)
   
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+    const newQuestion = await FetchQuizQuestions(
+      TOTAL_QUESTION,
+      Difficulty.HARDD
+    );
 
+    setQuestions(newQuestion);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -31,17 +52,25 @@ function App() {
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startTrivia}> Start</button>
-      <p className="score">Score</p>
-      <p>Loading Question ... </p>
-      {/* <QuestinCard 
+      
+      {gameOver || userAnswers.length ===  TOTAL_QUESTION ? (
+        <button className="start" onClick={startTrivia}> Start</button>
+      ): null}
+
+      {!gameOver ? <p className="score">Score</p> : null}
+      {loading && <p>Loading Question ... </p>}
+      
+      {!gameOver && !loading && (
+        <QuestinCard
           questionNo={number + 1}
           totalQuestions={TOTAL_QUESTION}
           question={questions[number].question}
           answers={questions[number].answers}
-          userAnswer={userAnswer ? userAnswer[number]  : undefined}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
           callback={checkAnswer}
-        /> */}
+        />
+      )}
+
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
